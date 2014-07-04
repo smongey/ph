fc90a349843edb5f9815c28e852c54b9e8c9a960
Sean Mongey <sm@smongey.com>
2014-07-04 12:37:37 +0100

    add hash url for #intern to open

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,13 +2,15 @@ window.addEventListener('load', function() {
     FastClick.attach(document.body);
 }, false);
 
-$('#workhere').on('click', function(){
+$('#workhere').on('click', function(e){
+	e.preventDefault();
 	if (!$('.main').hasClass('popped')){
 		$('.main, .job').addClass('popped');
 	} 
 });
 
-$('.job').on('click', function(){
+$('.job').on('click', function(e){
+	e.preventDefault();
 	$('.main, .job').removeClass('popped');
 });
 
@@ -17,3 +19,12 @@ $('body').css('opacity', '0' );
 $(window).load(function(){
 	$('body').animate({'opacity' : 1}, 300);
 });
+
+if(window.location.hash == '#intern') {
+	setTimeout(function(){
+		if (!$('.main').hasClass('popped')){
+			$('.main, .job').addClass('popped');
+		}
+	}, 1000)
+	
+}
